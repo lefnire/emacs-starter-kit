3d33105286fb1915f14bc58f7934c74fd5bc3ece
Tyler Renelle <tylerrenelle@gmail.com>
2011-06-23 10:09:28 -0700

    org-mode update, removing some unnecessaries

--- lefnire.el
+++ lefnire.el
@@ -120,9 +120,9 @@
 ;; The following lines are always needed.  Choose your own keys.
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
+;; (global-set-key "\C-cl" 'org-store-link)
+;; (global-set-key "\C-ca" 'org-agenda)
+;; (global-set-key "\C-cb" 'org-iswitchb)
 
 ;;;Custom
 (setq org-todo-keywords '((sequence "TODO" "WAITING" "|" "DONE")))
